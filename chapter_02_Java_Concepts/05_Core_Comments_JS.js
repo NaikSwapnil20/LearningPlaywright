bb753976c5f5b6d9f4baedcd3d99df14caf71a38
NaikSwapnil20 <naikswapnil020@gmail.com>
2026-02-21 00:06:25 +0530

    changes

---
+++ chapter_02_Java_Concepts/05_Core_Comments_JS.js
@@ -0,0 +1,13 @@
+// This is comment. -This code will not be execute
+console.log("Hello");
+/** 
+ *  This is multi line
+ *  Author : Swapnil Naik
+ *  Date : 21-Feb-2026
+ **/
+
+/* 
+ *  This is multi line
+ *  Author : Swapnil Naik
+ *  Date : 21-Feb-2026
+ */
---
+++ chapter_02_Java_Concepts/08_Lab.js
@@ -0,0 +1,5 @@
+let name = "Swapnil";
+console.log(name);
+
+// Ctlr + / - Windows for comments
+// CMD + / 
